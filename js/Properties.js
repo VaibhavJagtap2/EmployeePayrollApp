429cd78696f96c62cbf4cbc9b815c0c4eca5e104
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-07-03 00:22:05 +0530

    Add:Use Templete litrel

---
+++ js/Properties.js
@@ -0,0 +1,5 @@
+  
+let site_properties = {
+    home_page : "C:\Users\Admin\EmpPayRollApp\pagesC\HomePageUI.html",
+    add_emp_payroll_page : "C:\Users\Admin\EmpPayRollApp\pagesC\EmpPayRoll.html"
+};
