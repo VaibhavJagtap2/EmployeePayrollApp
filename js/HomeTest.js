be76b421bc90b3534278978bf3d579753d33b0e0
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-07-04 11:06:04 +0530

    Add:vie Data From JSON

--- js/HomeTest.js
+++ js/HomeTest.js
@@ -3,32 +3,65 @@ window.addEventListener('DOMContentLoaded',(event)=>{
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
-    <tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>
-    </tr>
-    <tr>
-        <td><img class="profile" alt="" src="C:/Users/Admin/EmpPayRollApp/assets/profile-images/Ellipse -2.png">
-    </td>
-    <td>Vaibhav</td>
-    <td>Male</td>
-    <td><div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div></td>
-    <td>450000</td>
-    <td>1 Nov 2020</td>
-    <td>
-        <img id="1" onclick="remove(this)" alt="delete"
-            src="C:/Users/Admin/EmpPayRollApp/assets/icons/delete-black-18dp.svg">
-        <img id="1" alt="edit" onclick="update(this)" 
-            src="C:/Users/Admin/EmpPayRollApp/assets/icons/create-black-18dp.svg">
-    </td>
-    </tr>
-`;
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"
+                        +"<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    let innerHtml = `${headerHtml}`;
+    let employeePayrollList = createEmployeePayrollJSON();
+    for(const employeePayrollData of employeePayrollList){
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" alt="" src="${employeePayrollData._profilePic}">
+        </td>
+        <td>${employeePayrollData._name}</td>
+        <td>${employeePayrollData._gender}</td>
+        <td>${getDeptHtml(employeePayrollData._department)}</td>
+        <td>${employeePayrollData._salary}</td>
+        <td>${employeePayrollData._startDate}</td>
+        <td>
+            <img id="1" onclick="remove(this)" alt="delete"
+                src="C:/Users/Admin/EmpPayRollApp/assets/icons/delete-black-18dp.svg">
+            <img id="1" alt="edit" onclick="update(this)" 
+                src="C:/Users/Admin/EmpPayRollApp/assets/icons/create-black-18dp.svg">
+        </td>
+        </tr>
+    `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let employeePayrollListLocal = [
+        {
+            _name: "Vaibhav Jagtap",
+            _gender: "M",
+            _department: [
+                "Engineering",
+                "Finance"
+            ],
+            _salary: "450000",
+            _startDate: "13 Oct 2019",
+            _note: '',
+            _profilePic: "C:/Users/Admin/EmpPayRollApp/assets/profile-images/Ellipse -2.png"
+        },
+        {
+            _name: "Suraj",
+            _gender: "M",
+            _department: [
+                "Sales"
+            ],
+            _salary: "300000",
+            _startDate: "24 Dec 2020",
+            _note: '',
+            _profilePic: "C:/Users/Admin/EmpPayRollApp/assets/profile-images/Ellipse -4.png"
+        }
+    ];
+    return employeePayrollListLocal;
+}
+
+const getDeptHtml = (deptList) =>{
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml}<div class = 'dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
--- js/Properties.js
+++ js/Properties.js
@@ -1,5 +1,5 @@
   
 let site_properties = {
-    home_page : "C:\Users\Admin\EmpPayRollApp\pagesC\HomePageUI.html",
-    add_emp_payroll_page : "C:\Users\Admin\EmpPayRollApp\pagesC\EmpPayRoll.html"
+    home_page : "C:/Users/Admin/EmpPayRollApp/pagesC/HomePageUI.html",
+    add_emp_payroll_page : "C:/Users/Admin/EmpPayRollApp/pagesC/EmpPayRoll.html"
 };
