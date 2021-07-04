20d62424d44586fd6a783defd8f5ebd6476bb8f5
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-07-04 11:38:06 +0530

    Add: TO Local Storage

--- js/HomeTest.js
+++ js/HomeTest.js
@@ -1,16 +1,20 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded',(event)=>{
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
 });
 
 const createInnerHtml = () => {
+    if(empPayrollList.length==0) return;
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"
                         +"<th>Salary</th><th>Start Date</th><th>Actions</th>";
     let innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJSON();
-    for(const employeePayrollData of employeePayrollList){
+    //let employeePayrollList = createEmployeePayrollJSON();
+    for(const employeePayrollData of empPayrollList){
         innerHtml = `${innerHtml}
         <tr>
-            <td><img class="profile" alt="" src="${employeePayrollData._profilePic}">
+            <td><img class="profile" alt="" src="${employeePayrollData._image}">
         </td>
         <td>${employeePayrollData._name}</td>
         <td>${employeePayrollData._gender}</td>
@@ -21,41 +25,12 @@ const createInnerHtml = () => {
             <img id="1" onclick="remove(this)" alt="delete"
                 src="C:/Users/Admin/EmpPayRollApp/assets/icons/delete-black-18dp.svg">
             <img id="1" alt="edit" onclick="update(this)" 
-                src="C:/Users/Admin/EmpPayRollApp/assets/icons/create-black-18dp.svg">
+                src="C:Users/Admin/EmpPayRollApp/assets/icons/create-black-18dp.svg">
         </td>
         </tr>
     `;
     }
     document.querySelector('#table-display').innerHTML = innerHtml;
-}
-
-const createEmployeePayrollJSON = () => {
-    let employeePayrollListLocal = [
-        {
-            _name: "Vaibhav Jagtap",
-            _gender: "M",
-            _department: [
-                "Engineering",
-                "Finance"
-            ],
-            _salary: "450000",
-            _startDate: "13 Oct 2019",
-            _note: '',
-            _profilePic: "C:/Users/Admin/EmpPayRollApp/assets/profile-images/Ellipse -2.png"
-        },
-        {
-            _name: "Suraj",
-            _gender: "M",
-            _department: [
-                "Sales"
-            ],
-            _salary: "300000",
-            _startDate: "24 Dec 2020",
-            _note: '',
-            _profilePic: "C:/Users/Admin/EmpPayRollApp/assets/profile-images/Ellipse -4.png"
-        }
-    ];
-    return employeePayrollListLocal;
 }
 
 const getDeptHtml = (deptList) =>{
@@ -64,4 +39,10 @@ const getDeptHtml = (deptList) =>{
         deptHtml = `${deptHtml}<div class = 'dept-label'>${dept}</div>`
     }
     return deptHtml;
+}
+
+const getEmployeePayrollDataFromStorage = () => {
+    console.log(localStorage.getItem('EmployeePayrollList'));
+    return localStorage.getItem('EmployeePayrollList')?
+                                JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
 }
