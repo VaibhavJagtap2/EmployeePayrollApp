19d97a817522d6bf4b2f9840518fa71e3291d597
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-07-06 14:01:01 +0530

    Add:Curd Operation 1

---
+++ js/site_properties.js
@@ -0,0 +1,5 @@
+  
+let site_properties = {
+    home_page : "C:/Users/Admin/EmpPayRollApp/pagesC/home.html",
+    add_emp_payroll_page : "C:/Users/Admin/EmpPayRollApp/pagesC/employeePayrollForm.html"
+};
